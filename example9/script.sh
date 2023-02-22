7151b80394ea6238248d22df7a746dd4ed2b6ed5
youssef baddi <baddiyoussef85@gmail.com>
2023-02-22 10:20:43 -0500

    start for loop

--- example9/script.sh
+++ example9/script.sh
@@ -1,13 +1,13 @@
-for var in 1 2 3 4 hello world
-do
-  echo $var
-done
-
-
-for var1 in file[123] file[456] file[789]
-do
-  echo $var1
-done
+#for var in 1 2 3 4 hello world
+#do
+#  echo $var
+#done
+#
+#
+#for var1 in file[123] file[456] file[789]
+#do
+#  echo $var1
+#done
 
 # Exercice 1
 # creer un script qui calcule le factorielle
@@ -22,12 +22,77 @@ done
 # fact(n) = fact(n-1) * n
 # fact(5) = 1 * 2 * 3 * 4 * 5
 
+#if [ $# -eq 0 ]
+#then
+#  read -p "veuillez entrer uen valeur " num
+#else
+#  num=$1
+#fi
+#
+#resultat=1
+#for((i=2;i<=$num;i++))
+#do
+#  echo $i
+#  resultat=$(( $resultat * $i ))
+#done
+#
+#echo $resultat
+#
+#resultat2=1
+#compteur=$num
+#while [ $compteur -gt 1 ]
+#do
+#  resultat2=$(( resultat2 * $compteur ))
+#  compteur=$(( $compteur - 1 ))
+#done
+#echo $resultat2
+
+
+
 # Exercice 2
 # creer un script qui calcule le nombre de fichier
 # le nombre de repertoire, le nombre de fichier excutable
 # d'un repertoire passer en argument
 # si il y a pas d'argument on demande a l'utilisateur
 # le dossier en question
+
+#echo $1
+if [ $# -eq 0 ]
+then
+  read -p "veuillez entrer un repertoir " rep
+else
+  rep=$1
+fi
+
+
+num_file=0
+num_folder=0
+num_exec=0
+#echo $rep
+cd $1
+for f in *
+do
+  if [ -d $f ]; then
+#    echo "$f est un dossier"
+    num_folder=$(( $num_folder + 1 ))
+    fi
+  if [ -f $f ]; then
+#    echo "$f est un fichier"
+    num_file=$(( num_file + 1 ))
+    fi
+  if [ -x $f ]; then
+#    echo "$f est un excutable"
+    num_exec=$(( num_exec + 1 ))
+    fi
+#   echo "========"
+done
+
+
+echo "folder = $num_folder"
+echo "fichier = $num_file"
+echo "executable = $num_exec"
+
+
 
 # Exercice 3
 # creer un script qui affiche en inverse le resultat de la
